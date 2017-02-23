e2267c506b9538bc100d0400a56d6ea92f1a3bbb
hoangvm <hoangvmh@gmail.com>
2017-02-24 00:11:03 +0900

    Add arrow button in ActionBar

--- app/src/main/java/jp/co/zenrin/music/play/ControllerActivity.java
+++ app/src/main/java/jp/co/zenrin/music/play/ControllerActivity.java
@@ -1,11 +1,13 @@
 package jp.co.zenrin.music.play;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class ControllerActivity extends AppCompatActivity {
+public class ControllerActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,16 @@ public class ControllerActivity extends AppCompatActivity {
         View view =getSupportActionBar().getCustomView();
         TextView txtView = (TextView) view.findViewById(R.id.title_action_bar);
         txtView.setText(getResources().getString(R.string.txt_controller));
+        // Add back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Toast.makeText(getApplicationContext(), "Tap on arrow", Toast.LENGTH_SHORT);
+    }
+
+
 }
