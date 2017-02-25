2a6685889f77f5f27d645abc5fc28f20c074efee
hoangvm <hoangvmh@gmail.com>
2017-02-25 23:46:24 +0900

    Play service music

--- app/src/main/java/jp/co/zenrin/music/player/MusicPlayActivity.java
+++ app/src/main/java/jp/co/zenrin/music/player/MusicPlayActivity.java
@@ -1,19 +1,28 @@
 package jp.co.zenrin.music.player;
 
+import android.content.ComponentName;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.PersistableBundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+import jp.co.zenrin.music.common.HondaConstants;
 import jp.co.zenrin.music.service.MediaPlayerService;
+import jp.co.zenrin.music.zdccore.HondaSharePreference;
 import jp.co.zenrin.music.zdccore.MusicController;
 import jp.co.zenrin.music.zdccore.SongAdapter;
 import jp.co.zenrin.music.zdccore.Track;
@@ -31,7 +40,7 @@ public class MusicPlayActivity extends AppCompatActivity {
     private ListView songView;
 
     // Service
-    private MediaPlayerService mediaPlayerService;
+    private MediaPlayerService player;
     private Intent playIntent;
 
     // Binding
@@ -44,11 +53,24 @@ public class MusicPlayActivity extends AppCompatActivity {
     private boolean pause = false;
     private boolean playBackPause = false;
 
+    private Button btnPrevious;
+    private Button btnPlay;
+    private Button btnPause;
+    private Button btnNext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_play);
+        btnPrevious = (Button) findViewById(R.id.btn_previous);
+        btnPlay = (Button) findViewById(R.id.btn_play);
+        btnPause = (Button) findViewById(R.id.btn_pause);
+        btnNext = (Button) findViewById(R.id.btn_next);
+        btnPrevious.setOnClickListener(mOnclick);
+        btnPlay.setOnClickListener(mOnclick);
+        btnPause.setOnClickListener(mOnclick);
+        btnNext.setOnClickListener(mOnclick);
         // Retrieve list view
         songView = (ListView) findViewById(R.id.song_list);
 
@@ -69,10 +91,23 @@ public class MusicPlayActivity extends AppCompatActivity {
         // Create and set adapter
         SongAdapter songAdt = new SongAdapter(this, trackList);
         songView.setAdapter(songAdt);
-
-        // Setup controller
-
     }
+
+    //Binding this Client to the AudioPlayer Service
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            MediaPlayerService.MusicBinder binder = (MediaPlayerService.MusicBinder) service;
+            player = binder.getService();
+            serviceBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            serviceBound = false;
+        }
+    };
 
     //method to retrieve song info from device
     public void getSongList(){
@@ -110,6 +145,74 @@ public class MusicPlayActivity extends AppCompatActivity {
             }
             while (musicCursor.moveToNext());
         }
+        musicCursor.close();
     }
 
+    /**
+     *
+     * @param v
+     */
+    private View.OnClickListener mOnclick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            final int id = v.getId();
+            switch (id) {
+                case R.id.btn_previous:
+                    break;
+                case R.id.btn_play:
+                    playAudio(1);
+                    break;
+                case R.id.btn_pause:
+                    break;
+                case R.id.btn_next:
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (serviceBound) {
+            unbindService(serviceConnection);
+            //service is active
+            player.stopSelf();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putBoolean("serviceStatus", serviceBound);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        serviceBound = savedInstanceState.getBoolean("serviceStatus");
+    }
+
+
+    private void playAudio(int audioIndex) {
+        //Check is service is active
+        if (!serviceBound) {
+            //Store Serializable audioList to SharedPreferences
+            HondaSharePreference storage = new HondaSharePreference(getApplicationContext());
+            storage.storeTrackList(trackList);
+            storage.storeTrackIndex(audioIndex);
+
+            Intent playerIntent = new Intent(this, MediaPlayerService.class);
+            startService(playerIntent);
+            bindService(playerIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+        } else {
+            //Store the new audioIndex to SharedPreferences
+            HondaSharePreference storage = new HondaSharePreference(getApplicationContext());
+            storage.storeTrackIndex(audioIndex);
+
+            //Service is active
+            //Send a broadcast to the service -> PLAY_NEW_AUDIO
+            Intent broadcastIntent = new Intent(HondaConstants.BROADCAST_PLAY_NEW_TRACK);
+            sendBroadcast(broadcastIntent);
+        }
+    }
 }
